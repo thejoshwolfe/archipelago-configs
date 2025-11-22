4aeaa325243f30563d54bd4b3bcbada48aa4179b
Josh Wolfe <thejoshwolfe@gmail.com>
2025-11-22 10:35:08 -0500

    ap client for factorio server

--- cli.py
+++ cli.py
@@ -2,12 +2,11 @@
 
 __doc__ = """\
 Invokes the Archipelago tools with CLI ergonomics more suitable to stateless automation.
-
-TODO: management for host.yaml values/overrides?
 """
 
 import os, sys, subprocess
-import shutil, tempfile
+import shutil, shlex, tempfile
+import re, json
 
 def main():
     import argparse
@@ -38,6 +37,21 @@ def main():
         "Path(s) to player .yaml files. "
         "These get copied into a tmp dir and given to Generate.py '--player_files_path'. ")
 
+    sub_parser = subparsers.add_parser("factorio", help=
+        "How I, a NixOS user, invoke the AP client for Factorio, which runs the Factorio headless server in a docker container. "
+        "Requires docker and a downloaded standalone factorio installation. "
+        "First, run 'generate' and start the archipelago server, then run this command. "
+        "Use the /connect command to connect to the archipelago game, which automatically launches the factorio server locally. "
+        "Once the factorio server is running, launch the factorio gui normally (via steam or whatever), and connect to localhost.")
+    sub_parser.add_argument("--factorio", metavar="/path/to/standalone/factorio", required=True, help=
+        "Download it from https://factorio.com/download . "
+        "The dir should contain bin/, data/, etc.")
+    sub_parser.add_argument("--mod", metavar="/path/to/AP-*.zip", required=True, help=
+        "The mod .zip produced by the 'generate' command. It's got your name in the file name.")
+    sub_parser.add_argument("--server-dir", required=True, help=
+        "The cwd for the factorio server. "
+        "The save file called Archipelago.zip goes there, and this script throws stuff in there as well.")
+
     args = parser.parse_args()
 
     if args.cmd == "update":
@@ -46,6 +60,8 @@ def main():
         do_init(args.repo)
     elif args.cmd == "generate":
         do_generate(args.repo, args.output_zip, args.output_dir, args.seed, args.player_yaml)
+    elif args.cmd == "factorio":
+        do_factorio(args.repo, args.mod, args.factorio, args.server_dir)
     else: assert False
 
 def do_update(repo):
@@ -128,9 +144,67 @@ def do_generate(repo, output_zip_path, output_dir, seed, player_yamls):
             shutil.copy(tmp_output_zip_path, output_zip_path)
         else: assert False
 
-def ap_cmd(script, *args, suppress_auto_install=True, input=b'', repo):
-    """ cwd is always repo. you gotta give absolute paths as args if you want them to work. """
-    python_exe = os.path.join(repo, ".venv", "bin", "python")
+
+def do_factorio(repo, mod_source_path, factorio_root, server_dir):
+    if not os.access(os.path.join(factorio_root, "bin/x64/factorio"), os.X_OK):
+        sys.exit("ERROR: does not appear to be a factorio root: " + repr(factorio_root))
+    # example name: AP-77091154303292394091-P1-josh_0.6.5.zip
+    ap_mod_name = re.match(r'^(AP-.*)_\d+\.\d+\.\d+\.zip$', os.path.basename(mod_source_path)).group(1)
+
+    try: os.mkdir(server_dir)
+    except FileExistsError: pass
+
+    # Mods
+    mods_dir = os.path.join(server_dir, "mods")
+    try:
+        os.mkdir(mods_dir)
+    except FileExistsError:
+        shutil.rmtree(mods_dir)
+        os.mkdir(mods_dir)
+    shutil.copy(mod_source_path, mods_dir + "/")
+    mod_list = {"mods": [
+        # These are the defaults that ship with space age
+        {"name": "base", "enabled": True},
+        {"name": "elevated-rails", "enabled": True},
+        {"name": "quality", "enabled": True},
+        {"name": "space-age", "enabled": True},
+    ]}
+    # AP is incompatible with space-age
+    for mod in mod_list["mods"]:
+        if mod["name"] == "space-age":
+            mod["enabled"] = False
+    # Enable the new mod.
+    mod_list["mods"].append({"name": ap_mod_name, "enabled": True})
+    with open(os.path.join(mods_dir, "mod-list.json"), "w") as f:
+        json.dump(mod_list, f, indent=2)
+
+    # Create a "factorio" executable that wraps invoking it through docker.
+    this_repo = os.path.dirname(os.path.abspath(__file__))
+    shutil.copy(os.path.join(this_repo, "deps/util/docker-apt-run"), os.path.join(server_dir, "docker-apt-run"))
+    factorio_in_docker_path = os.path.join(server_dir, "factorio-in-docker.sh")
+    with open(factorio_in_docker_path, "w") as f:
+        f.write("".join(line + "\n" for line in [
+            "#!/usr/bin/env bash",
+            'exec {0}/docker-apt-run -i ca-certificates --mount {1}:{1} -- {1}/bin/x64/factorio --mod-directory {2} "$@"'.format(
+                shlex.quote(os.path.abspath(server_dir)),
+                shlex.quote(os.path.abspath(factorio_root)),
+                shlex.quote(os.path.abspath(mods_dir)),
+            ),
+        ]))
+    chmod_x(factorio_in_docker_path)
+
+    # I believe host.yaml is the only way to configure this executable automatedly.
+    # (The yaml file gets formatted and filled out with default values when Launcher.py shuts down.)
+    host_j = {"factorio_options": {"executable": os.path.abspath(factorio_in_docker_path)}}
+    with open(os.path.join(server_dir, "host.yaml"), "w") as f:
+        json.dump(host_j, f)
+
+    ap_cmd("Launcher.py", "Factorio Client", "--", "--nogui", cwd=server_dir, input=None, repo=repo)
+
+def ap_cmd(script, *args, suppress_auto_install=True, input=b'', cwd=None, repo):
+    """ cwd defaults to repo """
+    if cwd == None:
+        cwd = repo
 
     env = os.environ.copy()
     if suppress_auto_install:
@@ -138,9 +212,16 @@ def ap_cmd(script, *args, suppress_auto_install=True, input=b'', repo):
     # We get deprecation warnings for importing pkg_resources. Not our problem, so suppress it.
     env["PYTHONWARNINGS"] = "ignore"
 
+    python_exe = os.path.join(repo, ".venv", "bin", "python")
     cmd = [python_exe, os.path.join(repo, script)]
     cmd.extend(args)
-    subprocess.run(cmd, check=True, env=env, input=input, cwd=repo)
+    subprocess.run(cmd, check=True, env=env, input=input, cwd=cwd)
+
+def chmod_x(path):
+    # This is like chmod +x, except that umask is preserved by copying the r bit to the x bit.
+    st_mode = os.stat(path).st_mode & 0o777
+    st_mode |= st_mode >> 2
+    os.chmod(path, st_mode)
 
 if __name__ == "__main__":
     main()
